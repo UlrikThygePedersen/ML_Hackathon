d0c05439ea593df5d2470b2c6abe610468d73df7
Bartimaeus25 <janportius9@gmail.com>
2026-05-20 14:05:10 +0200

    Automatically apply formatting changes

--- test_dummy_function.py
+++ test_dummy_function.py
@@ -1,10 +1,13 @@
 from dummy_function import multiply_by_two
 
+
 def test_multiply_by_two_positive_number():
     assert multiply_by_two(3) == 6
 
+
 def test_multiply_by_two_zero():
     assert multiply_by_two(0) == 0
+
 
 def test_multiply_by_two_negative_number():
     assert multiply_by_two(-4) == -8
