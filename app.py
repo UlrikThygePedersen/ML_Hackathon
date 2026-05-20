c5ad0fdea745a40984790aea6fc198538615001e
Jan Portius <JBXT@novonordisk.com>
2026-05-20 10:28:49 +0200

    First commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-st.header("Hello world!!!")
+st.header("Hello world!!!!")
 apps = st.slider("Number of apps", max_value=60, value=10)
 chart_data = pd.DataFrame({"y": [2**x for x in range(apps)]})
 st.bar_chart(
